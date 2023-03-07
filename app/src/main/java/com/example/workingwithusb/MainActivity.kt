38628fa117df3faaf3c89e6f73d4b597b1845ec2
Carlos Vargas Huaman <cvargas@godaddy.com>
2023-03-07 00:57:59 -0500

    first commit

---
+++ app/src/main/java/com/example/workingwithusb/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.workingwithusb
+
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val usbReceiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            if (ACTION_USB_PERMISSION == intent.action) {
+                synchronized(this) {
+                    val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        device?.apply {
+                            Log.i("MAINACTIVITY", "permission granted for device $device")
+                        }
+                    } else {
+                        Log.d("MAINACTIVITY", "permission denied for device $device")
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val permissionIntent = PendingIntent.getBroadcast(this, 0, Intent(ACTION_USB_PERMISSION), 0)
+        val filter = IntentFilter(ACTION_USB_PERMISSION)
+        registerReceiver(usbReceiver, filter)
+
+        fab.setOnClickListener {
+            nameTextView.text = "New Text"
+            val usbManager = getSystemService(Context.USB_SERVICE) as UsbManager
+            val deviceList: HashMap<String, UsbDevice> = usbManager.deviceList
+            deviceList.values.forEach { usbDevice ->
+                if (usbManager.hasPermission(usbDevice)) {
+                    Log.e("DEVICE", "HAS PERMISSION $usbDevice")
+                } else {
+                    usbManager.requestPermission(usbDevice, permissionIntent)
+                }
+            }
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    companion object {
+        private const val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
+    }
+}
